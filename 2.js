f7e401465f52f54dd9adb85f0e7d00c2d1c6593f
huitoutunao <lh_huitoutunao@163.com>
2022-01-30 18:02:03 +0800

    feat: f2-1

--- 2.js
+++ 2.js
@@ -1,6 +1,8 @@
 const h = 'hello world22'
 const g = 'huitoutunao222'
 
-const g2 = 'huitoutunao1'
+const g2 = 'huitoutunao11'
 const g3 = 'huitoutunao2'
 const g4 = 'huitoutunao3'
+
+const g5 = 'huitoutunao3'
